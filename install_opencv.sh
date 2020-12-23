05332ea219f8a5eddb671ec811ed9bd28c58d187
Leo Winiecki <leodwiniecki@outlook.com>
2020-12-23 14:28:28 -0800

    Script for installing opencv from source

---
+++ install_opencv.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+git clone https://github.com/opencv/opencv
+git -C opencv checkout 4.5.0
+# optionally
+git clone https://github.com/opencv/opencv_contrib
+git -C opencv_contrib checkout 4.5.0
+# optionally
+git clone https://github.com/opencv/opencv_extra
+git -C opencv_extra checkout 4.5.0
+
+mkdir build
+cd build
+
+cmake ..
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
+cmake --build .
+make
+
+sudo cmake --build . --target install
